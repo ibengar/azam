2ff3354535739e8c6c1dd22d1b210656968e219f
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-31 23:40:22 +0800

    Add files via upload

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,5 +1,5 @@
 const linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
-export async function before(m, { conn, isAdmin, isBotAdmin }) {
+export async function before(m, { isAdmin, isBotAdmin }) {
     if (m.isBaileys && m.fromMe)
         return !0
     if (!m.isGroup) return !1
@@ -12,7 +12,7 @@ export async function before(m, { conn, isAdmin, isBotAdmin }) {
             const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
             if (m.text.includes(linkThisGroup)) return !0
         }
-        await conn.sendButton(m.chat, `*Group link detect!*${isBotAdmin ? '' : '\n\n_Bot not admin_  t_t'}`, packname + " - " + author, ['off antilink', '/disable antilink'], m)
+        await conn.sendButton(m.chat, `*Group link detect!*${isBotAdmin ? '' : '\n\n_Bot not admin_  t_t'}`, author, ['off antilink', '/disable antilink'], m)
         if (isBotAdmin && bot.restrict) {
             await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
         } else if (!bot.restrict) return m.reply('Owner disable auto kick!')
